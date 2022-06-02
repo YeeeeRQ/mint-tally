870f453ed340def12609ba674925f59623aa663b
Wox <YeeeeRQ@outlook.com>
2022-06-02 10:31:56 +0800

    在Money页面中添加标签区域

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -54,4 +54,33 @@ export default {
   name: "Money",
 };
 </script>
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+.tags{
+  font-size: 14px;
+  padding: 16px;
+  > .current{
+    display: flex;
+    > li{
+      background-color: #d9d9d9;
+      $h: 24px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 12px;
+      margin-right: 12px;
+    }
+  }
+  > .new{
+    padding-top: 16px;
+    button{
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
+</style>
