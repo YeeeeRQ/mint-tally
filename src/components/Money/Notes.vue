fc50fa3739565d77b6dd81d6c3cf59b2116b0f80
Wox <YeeeeRQ@outlook.com>
2022-06-02 17:54:58 +0800

    使用JS实现Types.vue 中选择支出或收入的功能

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,35 @@
+<template>
+    <!-- label.notes>span.name+input[type="text"] -->
+    <div>
+      <label class="notes">
+        <span class="name">备注</span>
+        <input type="text" placeholder="此处输入备注"/>
+      </label>
+    </div>
+</template>
+
+<script lang="ts" >
+export default {
+    name: "Notes"
+}
+</script>
+
+<style lang="scss" scoped>
+
+.notes{
+  background: #f5f5f5;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  .name{
+    padding-right: 16px;
+  }
+  input{
+    flex-grow: 1;
+    height: 64px;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
