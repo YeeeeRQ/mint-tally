5c88757f6b7317224652dcc08a27c8665dcd77f8
Wox <YeeeeRQ@outlook.com>
2022-06-03 19:02:44 +0800

    在Notes.vue中使用v-model

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,30 +1,40 @@
 <template>
-    <!-- label.notes>span.name+input[type="text"] -->
-    <div>
-      <label class="notes">
-        <span class="name">备注</span>
-        <input type="text" placeholder="此处输入备注"/>
-      </label>
-    </div>
+  <!-- label.notes>span.name+input[type="text"] -->
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input
+        type="text"
+        placeholder="此处输入备注"
+        v-model="desc"
+      />
+    </label>
+  </div>
 </template>
 
-<script lang="ts" >
-export default {
-    name: "Notes"
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Notes extends Vue {
+  desc = "";
+  onChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.desc = input.value;
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
-.notes{
+.notes {
   background: #f5f5f5;
   padding-left: 16px;
   display: flex;
   align-items: center;
-  .name{
+  .name {
     padding-right: 16px;
   }
-  input{
+  input {
     flex-grow: 1;
     height: 64px;
     background: transparent;
