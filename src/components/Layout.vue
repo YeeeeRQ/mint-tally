e0ccb6715912e967bf455d1a1b6b1bb39e922f4f
Wox <YeeeeRQ@outlook.com>
2022-06-15 17:53:10 +0800

    解决在国产浏览器（UC，QQ）下应用的UI展示

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -3,38 +3,28 @@
     <div class="content" :class="classPrefix && `${classPrefix}-content`" ref="pageContent" :style="contentStyle">
       <slot></slot>
     </div>
-    <Nav class="nav-wrapper" @update:clientHeight="onNavMounted"/>
+    <Nav class="nav-wrapper" @update:clientHeight="onNavMounted" />
   </div>
 </template>
 
 <script lang="ts" >
 
-
 import { Vue, Component, Prop } from "vue-property-decorator"
 
 @Component
 export default class Layout extends Vue {
-  @Prop({default:'', type:String}) classPrefix?:'';
+  @Prop({ default: '', type: String }) classPrefix?: '';
   contentStyle = {
-    // border: "1px solid red",
     height: "100%",
   }
   beforeCreate() {
     console.log(window.visualViewport.width + ' * ' + window.visualViewport.height)
   }
 
-  onNavMounted(height: number){
-    // console.log(height);
-    const pageContent = this.$refs.pageContent as HTMLDivElement ;
+  onNavMounted(height: number) {
+    const pageContent = this.$refs.pageContent as HTMLDivElement;
     this.contentStyle.height = (pageContent.clientHeight - height).toString() + 'px';
   }
-  // mounted(){
-    // const navWrapper = this.$refs.navWrapper as HTMLDivElement ;
-
-    // console.log(this.$refs.pageContent);
-    // console.log(pageContent.clientHeight);
-    // console.log(navWrapper.clientHeight);
-  // }
 }
 </script>
 
@@ -44,20 +34,16 @@ export default class Layout extends Vue {
   display: flex;
   flex-direction: column;
   height: 100vh;
-  // position: fixed;
-  // bottom: 0;
-  // width: 100%;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
 }
 
 .content {
-  // border: 2px solid blue;
-
-  // flex-grow: 1;
   overflow: auto;
   position: fixed;
-  top:0;
+  top: 0;
   width: 100%;
-  // height: 762px;
 }
 
 .nav-wrapper {
